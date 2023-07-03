0455fb2d05fd29d7cfbfc8518e44dac437a36db7
Rafael Augusto <rafaelaugusto.developer@gmail.com>
2023-07-03 15:49:18 -0300

    feat: commit initial

---
+++ src/utils/env.ts
@@ -0,0 +1,13 @@
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+export default {
+  port: process.env.PORT,
+  db: {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    pass: process.env.DB_PASS,
+  },
+}
